9e7b6039121c6255582fe63839c4be8724621ba1
Joel Oliveira <joeloliveira1979@gmail.com>
2016-02-01 19:02:27 +0100

    Initial Commit

---
+++ Notificare/includes/class-notificare-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://notifica.re
+ * @since      1.0.0
+ *
+ * @package    Notificare
+ * @subpackage Notificare/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Notificare
+ * @subpackage Notificare/includes
+ * @author     Joel Oliveira <joel@notifica.re>
+ */
+class Notificare_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ Notificare/includes/class-notificare-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://notifica.re
+ * @since      1.0.0
+ *
+ * @package    Notificare
+ * @subpackage Notificare/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Notificare
+ * @subpackage Notificare/includes
+ * @author     Joel Oliveira <joel@notifica.re>
+ */
+class Notificare_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
